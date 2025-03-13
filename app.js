4f1d329bbd78f3b8b555bb7906a6144a561f0a21
Brendan Vuu <122244740+BrendanMVuu@users.noreply.github.com>
2025-03-13 05:43:52 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -193,17 +193,18 @@ dropZone.addEventListener('drop', handleDrop);
 playPauseButton.addEventListener('click', togglePlayPause);
 loopButton.addEventListener('click', toggleLoop);
 volumeSlider.addEventListener('input', updateVolume);
+volumeSlider.addEventListener('touchstart', updateVolume); // Add touch event listener for mobile devices
 volumeSlider.addEventListener('touchmove', updateVolume); // Add touch event listener for mobile devices
 pitchSlider.addEventListener('input', updatePitch);
 
 // Scroll event listener to handle footer fade in/out
 window.addEventListener('scroll', () => {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop && isFooterVisible) {
+    if (scrollTop > 0 && isFooterVisible) {
         // Scrolling down
         footer.style.opacity = '0';
         isFooterVisible = false;
-    } else if (scrollTop < lastScrollTop && !isFooterVisible) {
+    } else if (scrollTop === 0 && !isFooterVisible) {
         // Scrolling up
         footer.style.opacity = '1';
         isFooterVisible = true;
